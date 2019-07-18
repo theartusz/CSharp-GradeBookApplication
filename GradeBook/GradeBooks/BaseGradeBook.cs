fede4116d990e45c40caa24bb94668560ebcaf2a
theartusz <artureczek_5@live.com>
2019-07-18 10:48:50 +0200

    step 45 override statistics

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -54,6 +54,11 @@ namespace GradeBook.GradeBooks
             student.AddGrade(score);
         }
 
+        internal void CalculateStudentStatistics()
+        {
+            throw new NotImplementedException();
+        }
+
         public void RemoveGrade(string name, double score)
         {
             if (string.IsNullOrEmpty(name))
